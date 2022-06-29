32aaeb437948885c124f1a8772623994cff83a7b
Muizah <mapampa7@gmail.com>
2022-06-29 06:48:12 +0100

    add some animations

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-// Promise Do two thing
-// either reject the offer or resolve
-// function timeHandler() {
-//   console.log("5 second Timeout");
-// }
-// const pro = new Promise((resolve, reject) => {
-//   if (window.innerWidth > 800) {
-//     return resolve("Resolved");
-//   }
-//   return reject("Not Desktop");
-// });
-// pro
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => console.log(error));
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((error) => console.log(error));
-
-// Fetch Countries data from Web API
 const CardContainer = document.querySelector(".card-container");
 
-// var cardHtmls = ``;
-// var price = 0;
-// while (price <= 10) {
-//   price = price + 1;
-// }
-// for (let i = 0; i <= 6; i++) {
-//   cardHtmls = cardHtmls + cardHtml;
-// }
-// Array
 let countryData = [];
 const fetchData = fetch("https://restcountries.com/v3.1/all");
 fetchData
   .then((response) => response.json())
   .then((response) => {
-    //   Fetch the countries
     countryData = response;
     updateDom(response);
   })
   .catch((error) => console.log(error));
 
-// setTimeout(() => {
-//   console.log(countryData, "TimeOut");
-// }, 0);
 function updateDom(response) {
   var countries = "";
-
   for (var index = 0; index < response.length; index++) {
     const countryName = response[index].name.common;
     const population = response[index].population;
@@ -134,12 +98,6 @@ function regionFn(regionsArray) {
     });
   });
 }
-
-// var mode = document.querySelector("#mode_change");
-// // console.log(darkmode);
-// mode_change.addEventListener("click", () => {
-//   var darkmode =
-// });
 
 var mode = document.querySelector("#mode_change");
 mode.addEventListener("click", () => {
